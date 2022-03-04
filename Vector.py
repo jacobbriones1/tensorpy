0b021158b7d4f4cb35aae5e025f4128386bbc830
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2022-03-03 21:14:57 -0700

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -4,12 +4,11 @@ General Vector object defining properties of a vector and operations between vec
 Written by Jacob Briones
 """
 
-from math import acos
 import string
+from math import acos
 
 
 class Vector:
-    
     def __init__(self, vector, name=None):
         if name:
             self.name = name
@@ -20,22 +19,17 @@ class Vector:
         self.var_type = type(vector)
         self.len = sum([vector[i]*vector[i] for i in range(self.dim)])
 
-        
     def __repr__(self):
-        """Representation of Vector object as a string"""
         
-        rep = 'Vector([name: {}, dim:{}, components: ['.format(self.name,self.dim)
+        rep = 'Vector([components: ['
         for i in range(self.dim):
             rep += str(self.components[i])
             if i != self.dim -1:
                 rep += ', '
-        rep += '] , len: {}]'.format(self.len)
+        rep += '] , len: {}, name: {}, dim:{}, )'.format(self.len, self.name, self.dim)
         return rep
 
-    
     def __eq__(self, other):
-        """Check if two vectors are equal"""
-        
         if other is None:
             return False
         
@@ -52,10 +46,8 @@ class Vector:
         
         else:
             return None
-        
+
     def __add__(self, other):
-        """Addition of two vectors"""
-        
         if other.name == '' or self.name == '':
             new_name = ''
         else:
@@ -68,66 +60,79 @@ class Vector:
             raise ValueError('dimensions must match')
         return self.__class__(vector=new, name=new_name)
 
-    
-    def __mul__(self, scalar):
-        """Scalar multiplication of a vector"""
-        
-        if not isinstance(scalar, list):
-            return self.__class__(vector = [scalar*self.components[i] for i in range(self.dim)],
-                                  name = self.name)
+    def __sub__(self, other):
+        if other.name == '' or self.name == '':
+            new_name = ''
+        else:
+            new_name = self.name + '-' + other.name
+            
+        if other.dim == self.dim:
+            new = [self.components[i] - other.components[i] for i in range(self.dim)]
 
-        
-    def __rmul__(self, scalar):
-        """Scalar Multiplication"""
+        else:
+            raise ValueError('dimensions must match')
+        return self.__class__(vector=new, name=new_name)
+
+
+    def __mul__(self, scalar, name=None):
+        if not not Name and self.name != '':
+            new_name = str(scalar) + '*' + self.name 
+            
+        else:
+            new_name = ''
+            
         if not isinstance(scalar, list):
             return self.__class__(vector = [scalar*self.components[i] for i in range(self.dim)],
-                                  name = self.name)
+                                  name = new_name)
+
+    def __rmul__(self, scalar, name=None):
+        if not name and self.name != '':
+            new_name = str(scalar) + '*' + self.name
             
+        else:
+            new_name = ''
             
+        if not isinstance(scalar, list):
+            return self.__class__(vector = [scalar*self.components[i] for i in range(self.dim)],name = new_name)
+            
+
     def dot(self, other):
-        """Dot product of two vectors"""
-        
         if self.dim == other.dim and other.dim is not None:
             return sum([self.components[i]*other.components[i] for i in range(self.dim)])
-        
         else:
             raise ValueError('Vectors must have same dimension')
 
-            
     def cross(self, other, name=None):
-        """Cross product of two vectors."""
-        
-        if name:
-            new_name = name
+        if not name and self.name != '':
+            new_name = self.name + '×' + other.name
         else:
             new_name = ''
+            
         
         if self.dim == other.dim:
             v = []
             for i in range(self.dim):
                 v.append(0)
                 for j in range(self.dim):
-                    if j < i or j > i:
+                    if j < i or j> i:
                         for k in range(self.dim):
                             if k < i or k > i:
                                 if k > j:
                                     v[i] += self.components[j]*other.components[k]
-                                    
                                 elif k < j:
                                     v[i] -= self.components[j]*other.components[k]
-            
         else:
             raise ValueError('Dimensions must match')
-            
         return self.__class__(vector=v, name=new_name)
 
     def angle(self, other):
-        """Angle between two vectors"""
-        
         if other.dim != self.dim or other is None:
             raise ValueError('Vectors must have same dimension')
 
         else:
+            
             return acos(
                 sum([(self.components[i]*other.components[i])for i in range(self.dim)])/(self.len*other.len))
+
+
 
